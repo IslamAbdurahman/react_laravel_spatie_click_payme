cf5d27b25676cf2f2b47183426b8036ec7c51cb4
islamabdurahman <abdurahmanislam304@gmail.com>
2026-01-18 19:46:52 +0500

    add telegram

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,22 +1,21 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
+import LanguageBar from '@/components/language';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { type BreadcrumbItem as BreadcrumbItemType } from '@/types';
-import LanguageBar from '@/components/language';
 
 export function AppSidebarHeader({ breadcrumbs = [] }: { breadcrumbs?: BreadcrumbItemType[] }) {
     return (
-        <div className="fixed top-0 z-50 block w-full dark:bg-black bg-white">
-            <header className="flex justify-between items-center h-14 px-6 border-b border-sidebar-border/50">
+        <div className="fixed top-0 z-50 block w-full bg-white dark:bg-black">
+            <header className="border-sidebar-border/50 flex h-14 items-center justify-between border-b px-6">
                 <div className="flex items-center gap-2">
                     <SidebarTrigger className="-ml-1" />
                     <Breadcrumbs breadcrumbs={breadcrumbs} />
                 </div>
                 <div className="flex-1"></div>
-                <div className="fixed top-0 right-0 p-3">
+                <div className="fixed top-0 right-0 p-3 md:right-30">
                     <LanguageBar />
                 </div>
             </header>
         </div>
-
     );
 }
